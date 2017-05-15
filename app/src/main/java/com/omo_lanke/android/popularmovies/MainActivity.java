de676a3659ad383bbe95bd82937805cdf732f6e8
Alli Abdulateef <alli5723@gmail.com>
2017-05-15 07:09:46 +0100

    Working on save scroll state

--- app/src/main/java/com/omo_lanke/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/omo_lanke/android/popularmovies/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
@@ -46,7 +48,6 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     @BindView(R.id.movieList)
     RecyclerView movieList;
 
-    Menu menu;
     public static int SELECTED_MENU = 0;
     public static String USER_LIST_TYPE = "list_type";
 
@@ -146,6 +147,62 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     public void changeTitle(String name){
         getSupportActionBar().setTitle(name);
     }
+
+//    @Override
+//    protected Parcelable onSaveInstanceState() {
+//        Parcelable superState = super.onSaveInstanceState();
+//        LayoutManager layoutManager = getLayoutManager();
+//        if(layoutManager != null && layoutManager instanceof LinearLayoutManager){
+//            mScrollPosition = ((LinearLayoutManager) layoutManager).findFirstVisibleItemPosition();
+//        }
+//        SavedState newState = new SavedState(superState);
+//        newState.mScrollPosition = mScrollPosition;
+//        return newState;
+//    }
+//
+//    @Override
+//    protected void onRestoreInstanceState(Parcelable state) {
+//        super.onRestoreInstanceState(state);
+//        if(state != null && state instanceof SavedState){
+//            mScrollPosition = ((SavedState) state).mScrollPosition;
+//            LayoutManager layoutManager = getLayoutManager();
+//            if(layoutManager != null){
+//                int count = layoutManager.getChildCount();
+//                if(mScrollPosition != RecyclerView.NO_POSITION && mScrollPosition < count){
+//                    layoutManager.scrollToPosition(mScrollPosition);
+//                }
+//            }
+//        }
+//    }
+//
+//    static class SavedState extends android.view.View.BaseSavedState {
+//        public int mScrollPosition;
+//        SavedState(Parcel in) {
+//            super(in);
+//            mScrollPosition = in.readInt();
+//        }
+//        SavedState(Parcelable superState) {
+//            super(superState);
+//        }
+//
+//        @Override
+//        public void writeToParcel(Parcel dest, int flags) {
+//            super.writeToParcel(dest, flags);
+//            dest.writeInt(mScrollPosition);
+//        }
+//        public static final Parcelable.Creator<SavedState> CREATOR
+//                = new Parcelable.Creator<SavedState>() {
+//            @Override
+//            public SavedState createFromParcel(Parcel in) {
+//                return new SavedState(in);
+//            }
+//
+//            @Override
+//            public SavedState[] newArray(int size) {
+//                return new SavedState[size];
+//            }
+//        };
+//    }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
