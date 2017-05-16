eb48c788f0681a02ebae0c3470935392e82322c2
Alli Abdulateef <alli5723@gmail.com>
2017-05-16 08:59:09 +0100

    Removed unneeded lines

--- app/src/main/java/com/omo_lanke/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/omo_lanke/android/popularmovies/MainActivity.java
@@ -163,62 +163,6 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         getSupportActionBar().setTitle(name);
     }
 
-//    @Override
-//    protected Parcelable onSaveInstanceState() {
-//        Parcelable superState = super.onSaveInstanceState();
-//        LayoutManager layoutManager = getLayoutManager();
-//        if(layoutManager != null && layoutManager instanceof LinearLayoutManager){
-//            mScrollPosition = ((LinearLayoutManager) layoutManager).findFirstVisibleItemPosition();
-//        }
-//        SavedState newState = new SavedState(superState);
-//        newState.mScrollPosition = mScrollPosition;
-//        return newState;
-//    }
-//
-//    @Override
-//    protected void onRestoreInstanceState(Parcelable state) {
-//        super.onRestoreInstanceState(state);
-//        if(state != null && state instanceof SavedState){
-//            mScrollPosition = ((SavedState) state).mScrollPosition;
-//            LayoutManager layoutManager = getLayoutManager();
-//            if(layoutManager != null){
-//                int count = layoutManager.getChildCount();
-//                if(mScrollPosition != RecyclerView.NO_POSITION && mScrollPosition < count){
-//                    layoutManager.scrollToPosition(mScrollPosition);
-//                }
-//            }
-//        }
-//    }
-//
-//    static class SavedState extends android.view.View.BaseSavedState {
-//        public int mScrollPosition;
-//        SavedState(Parcel in) {
-//            super(in);
-//            mScrollPosition = in.readInt();
-//        }
-//        SavedState(Parcelable superState) {
-//            super(superState);
-//        }
-//
-//        @Override
-//        public void writeToParcel(Parcel dest, int flags) {
-//            super.writeToParcel(dest, flags);
-//            dest.writeInt(mScrollPosition);
-//        }
-//        public static final Parcelable.Creator<SavedState> CREATOR
-//                = new Parcelable.Creator<SavedState>() {
-//            @Override
-//            public SavedState createFromParcel(Parcel in) {
-//                return new SavedState(in);
-//            }
-//
-//            @Override
-//            public SavedState[] newArray(int size) {
-//                return new SavedState[size];
-//            }
-//        };
-//    }
-
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         outState.putInt(USER_LIST_TYPE, SELECTED_MENU);
